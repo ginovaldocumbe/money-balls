b36de5bce54448264662c05470d65706f5f43138
ginovaldocumbe <92864360+ginovaldocumbe@users.noreply.github.com>
2025-05-14 20:49:50 +0200

    feat: implement entry deletion functionality with confirmation dialog and notification

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="q-pa-md">
     <q-list bordered separator>
-      <q-item v-for="entry in entries" :key="entry.id">
-        <q-item-section class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
-          {{ entry.name }}
-        </q-item-section>
+      <q-slide-item
+        v-for="entry in entries"
+        :key="entry.id"
+        @right="onEntrySlideRight($event, entry)"
+        right-color="negative"
+      >
+        <template v-slot:right>
+          <q-icon name="delete" />
+        </template>
+        <q-item>
+          <q-item-section class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
+            {{ entry.name }}
+          </q-item-section>
 
-        <q-item-section side class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
-          {{ useCurrencify(entry.amount) }}
-        </q-item-section>
-      </q-item>
+          <q-item-section side class="text-weight-bold" :class="useAmountColorClass(entry.amount)">
+            {{ useCurrencify(entry.amount) }}
+          </q-item-section>
+        </q-item>
+      </q-slide-item>
     </q-list>
   </div>
   <q-footer class="bg-transparent">
@@ -49,9 +59,11 @@
 
 <script setup>
 import { ref, computed, reactive } from 'vue'
-import { uid } from 'quasar'
+import { uid, useQuasar } from 'quasar'
 import { useCurrencify } from 'src/use/useCurrency.js'
 import { useAmountColorClass } from 'src/use/useAmountColorClass.js'
+
+const $q = useQuasar()
 
 const entries = ref([
   {
@@ -101,5 +113,36 @@ const addEntry = () => {
   const newEntry = Object.assign({}, addEntryForm, { id: uid() })
   entries.value.push(newEntry)
   addEntryFormReset()
+}
+
+const onEntrySlideRight = ({ reset }, entry) => {
+  $q.dialog({
+    title: 'Delete Entry',
+    message: `
+    Delete this entry? 
+    <div class='q-mt-sm text-weight-bold text-subtitle2 ${useAmountColorClass(entry.amount)}'>
+      ${entry.name} : ${useCurrencify(entry.amount)}
+      </div>
+    `,
+    html: true,
+    cancel: true,
+    persistent: true,
+  })
+    .onOk(() => {
+      deleteEntry(entry.id)
+    })
+    .onCancel(() => {
+      reset()
+    })
+}
+
+const deleteEntry = (entryId) => {
+  const index = entries.value.findIndex((entry) => entry.id === entryId)
+  entries.value.splice(index, 1)
+  $q.notify({
+    message:'Entry deleted successfully.',
+    position:'top',
+    color:'green'
+  })
 }
 </script>
